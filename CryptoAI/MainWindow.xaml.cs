6ecf3f8c42926b1322c180fda31e0e8d5c9e00e3
DavDavUdav <stepan05az@gmail.com>
2023-03-25 11:12:49 +0300

    Добавьте файлы проекта.

---
+++ CryptoAI/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptoAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btn_site_Click()
+        {
+
+        }
+
+        private void btn_site_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_language_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_License_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
